93c7628d61163a4a974854da4ac6527153b0e9a1
Lungpin Yeh <lungpin.yeh@gmail.com>
2014-03-16 07:13:39 +0000

    rails g mercury:install:images
    1. Refer to https://www.bountysource.com/issues/1072031-ar_paperclip_image-rb-erb-filename-problem?utm_campaign=plugin&utm_content=tracker%2F134071&utm_medium=issues&utm_source=github for fixing "Could not find "ar_paperclip_image.rb" in any of your source paths." error
    2. bundle install
    3. rake db:migrate

--- cms-after/config/routes.rb
+++ cms-after/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+    namespace :mercury do
+      resources :images
+    end
   mount Mercury::Engine => '/'
   root to: 'pages#index'
   resources :pages, only: [:index, :show]
